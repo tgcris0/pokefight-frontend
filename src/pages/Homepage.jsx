a8387d69e5641f2523ca690518bf6df24bcaebd1
GoeranLE <goeran.lehmann@gmail.com>
2020-11-10 10:27:23 +0100

    Add Background

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Background from '../image/bgtwo.jpg';
+import { Button } from '@material-ui/core';
+
+
+/// Goeran Background
+
+const sectionStyle = {
+	width: "100%",
+	height: "900px",
+	backgroundImage: `url(${Background})`
+  };
 
 const Homepage = () => {
 	return (
-		<div className="Homepage">
-			<h1>Homepage JSX</h1>
-		</div>
+		<div>
+		<Button variant="outlined">Change the ARENA</Button>
+         <section style={ sectionStyle }>
+      </section>
+      </div>
 	);
 };
 
